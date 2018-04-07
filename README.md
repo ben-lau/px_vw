1c3899ce474237c41dd872813b1f1251fcd3b265
BenLau <408736066@qq.com>
2018-04-07 18:21:38 +0800

    some bug fix and rename

---
+++ README.md
@@ -0,0 +1,25 @@
+# px_vw
+
+Convert your stylesheet unit from 'px' to 'vw'
+
+## Usage
+
+```
+$ npm install px_vw -g
+```
+```
+$ px_vw [options] <file>
+```
+```
+    Options:
+        -h, --help                      output usage information
+        -V, --version                   output the version number
+        -w, --baseWidth                 set your proto design width (default: 1080)
+        -a, --accuracy                  set the digital accurarcy of converted stylesheet (default: 4)
+        -o, --output [path]             the output file dirname
+```
+
+
+## License
+
+MIT
--- bin/px_vw.js
+++ bin/px_vw.js
@@ -1,12 +1,13 @@
-var commander = require("commander");
-var fs = require("fs-extra");
-var path = require("path");
-var chalk = require("chalk");
-var package = require("./../package.json");
-var Px2vw = require("./../lib/px_vw");
+const commander = require("commander");
+const fs = require("fs");
+const path = require("path");
+const chalk = require("chalk");
+const package = require("./../package.json");
+const Px_vw = require("./../lib/px_vw");
 
 commander.version(package.version)
-    .option("-w, --baseWidth [value]", "set your proto graph width", 1080)
+    .option("-w, --baseWidth [value]", "set your proto design width", 1080)
+    .option("-a, --accuracy [value]", "set the digital accurarcy of converted stylesheet", 4)
     .option("-o, --output [path]", "the output file dirname")
     .parse(process.argv);
 
@@ -22,26 +23,26 @@ if (commander.args.findIndex(function (filePath) {
     return false;
 }
 
-var config = {
-    baseWidth: commander.baseWidth
+const config = {
+    baseWidth: commander.baseWidth,
+    accuracy: commander.accuracy
 }
 
-var px2vw = new Px2vw(config);
+const px_vw = new Px_vw(config);
 
 commander.args.forEach(function (filePath) {
     if (path.extname(filePath) !== '.css') {
         return;
     }
 
-    var cssText = fs.readFileSync(filePath, { encoding: 'utf8' });
-    var outputPath = commander.output || path.dirname(filePath);
-    var fileName = path.basename(filePath);
+    const cssText = fs.readFileSync(filePath, { encoding: 'utf8' });
+    const outputPath = commander.output || path.dirname(filePath);
+    const fileName = path.basename(filePath);
 
-    var newCssText = px2vw.translate(cssText);
-    var newFileName = fileName.replace(".css", ".vw.css");
-    var newFilePath = path.join(outputPath, newFileName);
+    const newCssText = px_vw.convertCss(cssText);
+    const newFileName = fileName.replace(".css", ".vw.css");
+    const newFilePath = path.join(outputPath, newFileName);
 
-    fs.createFileSync(newFilePath);
     fs.writeFileSync(newFilePath, newCssText, { encoding: "utf8" });
     console.log(chalk.green.bold("your new stylesheet is in" + newFilePath));
 })
